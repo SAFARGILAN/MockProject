6d40e5ba7d809ad5dc4a4334514967c0f9eada05
ZeusSatake <gilansafar54@gmail.com>
2024-07-03 15:40:17 +0900

    武器当たり判定の設定

--- Source/MockProject/Public/Weapon/Weapon.h
+++ Source/MockProject/Public/Weapon/Weapon.h
@@ -9,6 +9,7 @@
 class UStaticMeshComponent;
 class AMainCharacter;
 class UBoxComponent;
+class USceneComponent;
 
 UCLASS()
 class MOCKPROJECT_API AWeapon : public AActor
@@ -30,8 +31,21 @@ protected:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UBoxComponent> WeaponCollisionBox;
+
+	
+
+
+
+	UFUNCTION(BlueprintCallable)
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceStart;
 
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* BoxTraceEnd;
+	
 };
